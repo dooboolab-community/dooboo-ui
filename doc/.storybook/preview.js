e4618ad7b0508b1a3464404e138a386da2bbf3cb
hyochan <dooboolab@gmail.com>
2021-05-31 16:35:50 +0900

    Revert "Remove react-native-web storybook"
    This reverts commit 56329052b56440d8dbf08164ea6f6ddbf9c2da0a.

---
+++ doc/.storybook/preview.js
@@ -0,0 +1,40 @@
+import { addParameters, addDecorator } from '@storybook/react';
+import {createElement} from 'react';
+
+addDecorator(createElement);
+
+// Option defaults:
+addParameters({
+  options: {
+    storySort: (a, b) => {
+      const sectionA = a[1].id.split('-')[0];
+      const sectionB = b[1].id.split('-')[0];
+      if (sectionA === sectionB) {
+        return a[0].localeCompare(b[0]);
+      }
+      const sectionMap = {
+        overview: 0, components: 1, packages: 2
+      };
+      return sectionMap[sectionA] - sectionMap[sectionB];
+    },
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    panelPosition: 'bottom',
+  },
+});
