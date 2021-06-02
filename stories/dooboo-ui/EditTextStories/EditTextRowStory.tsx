3b893279ee8188a85275a45885c0db5e010f1b45
hyochan <dooboolab@gmail.com>
2021-06-02 21:50:12 +0900

    Remove `Hr` in [EditText] stories
    They look ugly since [EditText] itself already has underline.

--- stories/dooboo-ui/EditTextStories/EditTextRowStory.tsx
+++ stories/dooboo-ui/EditTextStories/EditTextRowStory.tsx
@@ -1,4 +1,4 @@
-import {EditText, Hr, useTheme} from '../../../main';
+import {EditText, useTheme} from '../../../main';
 import React, {useState} from 'react';
 import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 
@@ -38,7 +38,6 @@ const EditTextRow = (): React.ReactElement => {
           onChangeText={(text) => onTextChanged('EMAIL', text)}
           style={{marginTop: 20, marginBottom: 52, paddingHorizontal: 36}}
         />
-        <Hr />
         <Text
           style={{
             fontSize: 18,
@@ -57,7 +56,6 @@ const EditTextRow = (): React.ReactElement => {
           onChangeText={(text) => onTextChanged('PASSWORD', text)}
           style={{marginTop: 20, marginBottom: 40, paddingHorizontal: 36}}
         />
-        <Hr />
         <Text
           style={{
             fontSize: 18,
@@ -75,7 +73,6 @@ const EditTextRow = (): React.ReactElement => {
           editable={false}
           style={{marginTop: 20, marginBottom: 40, paddingHorizontal: 36}}
         />
-        <Hr />
         <Text
           style={{
             fontSize: 18,
