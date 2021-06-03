4acd52ecd62fd28d488d8233124af9ed00178be0
hyochan <dooboolab@gmail.com>
2021-06-03 23:09:43 +0900

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,14 @@ process.env.TZ = 'Asia/Seoul';
 
 module.exports = {
   clearMocks: true,
-  preset: 'react-native',
+  preset: 'jest-expo',
   transform: {
     '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
   },
-  transformIgnorePatterns: ['node_modules/(?!(.*-)?react-(.*-)?native(-.*)?)'],
+  transformIgnorePatterns: [
+    // eslint-disable-next-line max-len
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
   modulePaths: ['<rootDir>'],
   moduleDirectories: ['node_modules'],
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
