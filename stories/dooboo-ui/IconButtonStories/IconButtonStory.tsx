cf6162624ed2036a713bf7fe2275aa262dbbdd15
hyochan <dooboolab@gmail.com>
2021-06-05 00:10:06 +0900

    Render IconButtonStory correctly in web

--- stories/dooboo-ui/IconButtonStories/IconButtonStory.tsx
+++ stories/dooboo-ui/IconButtonStories/IconButtonStory.tsx
@@ -1,5 +1,5 @@
-import {Hr, IconButton} from '../../../main';
-import {Text, View} from 'react-native';
+import {Hr, IconButton, useTheme} from '../../../main';
+import {Platform, Text, View} from 'react-native';
 
 import type {FC} from 'react';
 import {Icon} from '../../../main/Icon';
@@ -26,11 +26,12 @@ const Container = styled.View`
 `;
 
 const StyledIcon = styled(Icon)`
-  padding: 8px;
   color: ${({theme}) => theme.textContrast};
 `;
 
 const IconStory: FC = () => {
+  const {theme} = useTheme();
+
   const [fontsLoaded] = useFonts({
     IcoMoon: require('../../../main/Icon/doobooui.ttf'),
   });
@@ -38,78 +39,107 @@ const IconStory: FC = () => {
   if (!fontsLoaded) return <View />;
 
   return (
-    <ScrollContainer
-      contentContainerStyle={{
+    <View
+      style={{
+        backgroundColor: theme.background,
+        flex: 1,
         alignSelf: 'stretch',
       }}>
-      <Container>
-        <Text style={{fontSize: 18, marginTop: 24, marginBottom: 8}}>
-          Medium IconButton (default)
-        </Text>
-        <View
-          style={{
-            width: '100%',
-            marginTop: 40,
+      <ScrollContainer
+        style={{flex: 1}}
+        contentContainerStyle={{
+          alignSelf: 'stretch',
+          height: Platform.select({
+            web: '100%',
+          }),
+        }}>
+        <Container>
+          <Text
+            style={{
+              fontSize: 18,
+              marginTop: 24,
+              marginBottom: 8,
+              color: theme.text,
+            }}>
+            Medium IconButton (default)
+          </Text>
+          <View
+            style={{
+              width: '100%',
+              marginTop: 40,
 
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <IconButton icon={<StyledIcon size={24} name="moment-solid" />} />
-          <View style={{width: 8}} />
-          <IconButton icon={<StyledIcon size={24} name="add-light" />} />
-          <View style={{width: 8}} />
-          <IconButton icon={<StyledIcon size={24} name="chevron-right" />} />
-        </View>
-        <Hr style={{marginTop: 40}} />
-        <Text style={{fontSize: 18, marginTop: 24, marginBottom: 8}}>
-          Large IconButton
-        </Text>
-        <View
-          style={{
-            marginTop: 40,
-            width: '100%',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <IconButton icon={<StyledIcon size={24} name="moment-solid" />} />
+            <View style={{width: 8}} />
+            <IconButton icon={<StyledIcon size={24} name="add-light" />} />
+            <View style={{width: 8}} />
+            <IconButton icon={<StyledIcon size={24} name="chevron-right" />} />
+          </View>
+          <Hr style={{marginTop: 40}} />
+          <Text
+            style={{
+              fontSize: 18,
+              marginTop: 24,
+              marginBottom: 8,
+              color: theme.text,
+            }}>
+            Large IconButton
+          </Text>
+          <View
+            style={{
+              marginTop: 40,
+              width: '100%',
 
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <IconButton
-            size="large"
-            icon={<StyledIcon size={32} name="cross-light" />}
-          />
-          <View style={{width: 8}} />
-          <IconButton
-            size="large"
-            icon={<StyledIcon size={32} name="tile-light" />}
-          />
-        </View>
-        <Hr style={{marginTop: 40}} />
-        <Text style={{fontSize: 18, marginTop: 24, marginBottom: 8}}>
-          Small IconButton
-        </Text>
-        <View
-          style={{
-            marginTop: 40,
-            marginBottom: 100,
-            width: '100%',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <IconButton
+              size="large"
+              icon={<StyledIcon size={32} name="cross-light" />}
+            />
+            <View style={{width: 8}} />
+            <IconButton
+              size="large"
+              icon={<StyledIcon size={32} name="tile-light" />}
+            />
+          </View>
+          <Hr style={{marginTop: 40}} />
+          <Text
+            style={{
+              fontSize: 18,
+              marginTop: 24,
+              marginBottom: 8,
+              color: theme.text,
+            }}>
+            Small IconButton
+          </Text>
+          <View
+            style={{
+              marginTop: 40,
+              marginBottom: 100,
+              width: '100%',
 
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <IconButton
-            size="small"
-            icon={<StyledIcon size={16} name="cross-light" />}
-          />
-          <View style={{width: 8}} />
-          <IconButton
-            size="small"
-            icon={<StyledIcon size={16} name="tile-light" />}
-          />
-        </View>
-      </Container>
-    </ScrollContainer>
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <IconButton
+              size="small"
+              icon={<StyledIcon size={16} name="cross-light" />}
+            />
+            <View style={{width: 8}} />
+            <IconButton
+              size="small"
+              icon={<StyledIcon size={16} name="tile-light" />}
+            />
+          </View>
+        </Container>
+      </ScrollContainer>
+    </View>
   );
 };
 
