8eaf2eb421cdfdb01ddc9cf881faa7148108f440
hyochan <dooboolab@gmail.com>
2021-08-07 21:45:44 +0900

    Update test setup

--- test/setupTest.ts
+++ test/setupTest.ts
@@ -7,13 +7,14 @@ afterEach(cleanup);
 
 // Use fake system time.
 // Required for consistent snapshot testing.
+// jest-expo@42.1.0 no more need 'modern' param
 /**
  * TODO: `jest-expo` version 40 depends on `jest` version 25.
  * `jest.setSystemTime` function is added in `jest` version 26.
  * When `jest-expo` updates its dependency to `jest@26`,
  * change `Date.now = jest.fn()` to a `jest.setSystemTime` call.
  */
-jest.useFakeTimers('modern');
+jest.useFakeTimers();
 Date.now = jest.fn(() => new Date('2021-03-19T04:30:54.591Z').valueOf());
 
 process.on('unhandledRejection', (err) => {
