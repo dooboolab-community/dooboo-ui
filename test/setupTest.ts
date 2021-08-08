30d94910e20192904fa16a706fb4d1f6b19500ac
yujonglee <61503739+yujong-lee@users.noreply.github.com>
2021-08-08 20:37:13 +0900

    Rewrite [ButtonGroup] test (#53)
    As I mentioned in https://github.com/dooboolab/dooboo-ui/issues/42#issue-957791700, relying on snapshot testing is not good practice.
    
    But I don't want to make radical changes. So I left basic snapshot testing and add tests below it.

--- test/setupTest.ts
+++ test/setupTest.ts
@@ -1,5 +1,7 @@
 import {cleanup} from '@testing-library/react-native';
 
+import '@testing-library/jest-native/extend-expect';
+
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 
 // Cleanup after each case.
