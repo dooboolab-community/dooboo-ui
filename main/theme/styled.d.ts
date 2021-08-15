702e2329594b41827668667594ff5734ae735074
hyochan <dooboolab@gmail.com>
2021-08-15 15:07:39 +0900

    Detect orientation on media query

--- main/theme/styled.d.ts
+++ main/theme/styled.d.ts
@@ -3,6 +3,7 @@ import type {DoobooTheme} from './index';
 
 declare module '@emotion/react' {
   export interface Theme extends DoobooTheme {
+    isPortrait?: boolean;
     isMobile?: boolean;
     isTablet?: boolean;
     isDesktop?: boolean;
