5ffb1780aa01d71b31eb6613c92674b598afe5d2
9ummy <godante27@gmail.com>
2021-08-29 16:38:03 +0900

    Add [RadioGroup] demo components with labels

---
+++ docs/src/components/RadioGroup/WithLabels.tsx
@@ -0,0 +1,49 @@
+import {RadioGroup, ThemeProvider} from 'dooboo-ui';
+import React, {ReactElement, useState} from 'react';
+
+import {RadioButtonType} from 'dooboo-ui/RadioGroup/RadioButton';
+import styled from '@emotion/native';
+
+const Container = styled.SafeAreaView`
+  align-self: stretch;
+  background-color: ${({theme}) => theme.background};
+  padding: 15px 0;
+  align-items: center;
+`;
+
+const data = ['Dog', 'Cat', 'Cow', 'Horse'];
+const labels = ['Dog', 'Cat', 'Cow', 'Horse'];
+
+type Props = {
+  title?: string;
+  type?: RadioButtonType;
+};
+
+const RadioGroupStory = ({title, type}: Props): React.ReactElement => {
+  const [selectedValue, setSelectedValue] = useState<string>(data[0]);
+
+  return (
+    <Container>
+      <RadioGroup
+        title={title}
+        data={data}
+        type={type || 'primary'}
+        labels={labels}
+        selectedValue={selectedValue}
+        selectValue={(value) => setSelectedValue(value)}
+      />
+    </Container>
+  );
+};
+
+export const WithLabelsLight = ({title, type}: Props): ReactElement => (
+  <ThemeProvider initialThemeType="light">
+    <RadioGroupStory title={title} type={type} />
+  </ThemeProvider>
+);
+
+export const WithLabelsDark = ({title, type}: Props): ReactElement => (
+  <ThemeProvider initialThemeType="dark">
+    <RadioGroupStory title={title} type={type} />
+  </ThemeProvider>
+);
