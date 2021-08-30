95d8d26083eafc5549e9eaef7bb09d0b7de440c6
915dbfl <dbfl00915@naver.com>
2021-08-30 21:51:39 +0900

    Add CustomLoadingIndicator demo

---
+++ stories/dooboo-ui/LoadingIndicatorStories/CustomLoadingIndicator.tsx
@@ -0,0 +1,56 @@
+import {Animated, Easing, ImageStyle, StyleProp, View} from 'react-native';
+import React, {FC, useEffect, useState} from 'react';
+
+import {IC_MASK} from '../../Icon';
+
+type Props = {
+  style?: StyleProp<ImageStyle>;
+};
+
+const CustomLoadingIndicator: FC<Props> = ({style}) => {
+  const [spinAnim] = useState(new Animated.Value(0));
+
+  const spin = spinAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(spinAnim, {
+        toValue: 1,
+        duration: 1600,
+        easing: Easing.linear,
+        useNativeDriver: false,
+      }),
+    ).start();
+  });
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: 'white',
+
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Animated.Image
+        style={[
+          {
+            opacity: 0.95,
+            height: 56,
+            width: 56,
+            borderRadius: 28,
+            transform: [{rotate: spin}],
+          },
+          style,
+        ]}
+        source={IC_MASK}
+      />
+    </View>
+  );
+};
+
+export default CustomLoadingIndicator;
