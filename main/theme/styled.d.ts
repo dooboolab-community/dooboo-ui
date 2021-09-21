581167a20cdfb2a011e5d006a0b00bbd15f42231
Jayden <yujonglee@g.skku.edu>
2021-09-21 18:09:59 +0900

    Remove unecessary properties in Theme declaration (#121)

--- main/theme/styled.d.ts
+++ main/theme/styled.d.ts
@@ -2,10 +2,5 @@ import '@emotion/react';
 import type {DoobooTheme} from './index';
 
 declare module '@emotion/react' {
-  export interface Theme extends DoobooTheme {
-    isPortrait?: boolean;
-    isMobile?: boolean;
-    isTablet?: boolean;
-    isDesktop?: boolean;
-  }
+  export interface Theme extends DoobooTheme {}
 }
