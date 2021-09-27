8d0a600ce8a19a7b90d6e46c88d5549ff561bac1
hyochan <dooboolab@gmail.com>
2021-09-27 18:48:47 +0900

    Rename files with capitalizations

--- packages/Theme/index.tsx
+++ packages/Theme/index.tsx
@@ -1,2 +1,2 @@
-export {ThemeProvider, useTheme, withTheme} from './themeProvider';
+export {ThemeProvider, useTheme, withTheme} from './Themeprovider';
 export {DoobooTheme, ThemeType, dark, light} from './colors';
