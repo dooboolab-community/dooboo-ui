b4413e5066e7de76a9690a91d1d4926a90a5cada
hyochan <dooboolab@gmail.com>
2021-10-17 02:19:20 +0900

    Fix broken docs from #150

--- docs/src/components/Checkbox/Basic.tsx
+++ docs/src/components/Checkbox/Basic.tsx
@@ -1,6 +1,6 @@
-import {Checkbox, LoadingIndicator, useTheme} from 'dooboo-ui';
+import {Checkbox, LoadingIndicator} from 'dooboo-ui';
 import React, {FC, useState} from 'react';
-import {ThemeProvider, ThemeType} from '@dooboo-ui/theme';
+import {ThemeProvider, ThemeType, useTheme} from '@dooboo-ui/theme';
 
 import {View} from 'react-native';
 import {useFonts} from 'expo-font';
