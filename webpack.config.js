c7bf10b54cbd1fa8c1301090296d6647a28c3c20
hyochan <dooboolab@gmail.com>
2021-10-18 14:27:02 +0900

    wip: Fix gh-pages build error

--- webpack.config.js
+++ webpack.config.js
@@ -11,41 +11,10 @@ module.exports = async (env, argv) => {
     argv,
   );
 
-  // Remove existing rules about SVG and inject our own
-  // (Inspired by https://github.com/storybookjs/storybook/issues/6758#issuecomment-495598635)
-  config.module.rules = config.module.rules.map((rule) => {
-    if (rule.oneOf) {
-      let hasModified = false;
-
-      const newRule = {
-        ...rule,
-        oneOf: rule.oneOf.map((oneOfRule) => {
-          if (oneOfRule.test && oneOfRule.test.toString().includes('svg')) {
-            hasModified = true;
-
-            const test = oneOfRule.test.toString().replace('|svg', '');
-
-            return {...oneOfRule, test: new RegExp(test)};
-          } else return oneOfRule;
-        }),
-      };
-
-      // Add new rule to use svgr
-      // Place at the beginning so that the default loader doesn't catch it
-      if (hasModified)
-        newRule.oneOf.unshift({
-          test: /\.svg$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: '@svgr/webpack',
-            },
-          ],
-        });
-
-      return newRule;
-    } else return rule;
-  });
+  config.resolve.alias = {
+    'react-native': 'react-native-web',
+    '@storybook/react-native': '@storybook/react',
+  };
 
   return config;
 };
