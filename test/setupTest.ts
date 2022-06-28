48b03e92b0f64873c7e2597a97dae4cd16b8b5eb
hyochan <dooboolab@gmail.com>
2022-06-28 19:20:45 +0900

    Remove unnecessary `cleanup`

--- test/setupTest.ts
+++ test/setupTest.ts
@@ -2,13 +2,8 @@ import '@testing-library/jest-native/extend-expect';
 import 'jest-plugin-context/setup';
 import 'givens/setup';
 
-import {cleanup} from '@testing-library/react-native';
-
 jest.mock('@dooboo-ui/theme/useColorScheme', () => jest.fn());
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-
-// Cleanup after each case.
-afterEach(cleanup);
 
 process.on('unhandledRejection', (err) => {
   fail(err);
