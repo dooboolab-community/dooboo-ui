cf858ec602ef4823ce3b77438f53724d789bda1b
hyochan <dooboolab@gmail.com>
2022-06-28 18:51:45 +0900

    Extract nested components
    This is anti-pattern.

--- packages/AlertDialog/stories/DefaultStory.tsx
+++ packages/AlertDialog/stories/DefaultStory.tsx
@@ -28,105 +28,119 @@ interface ExampleButtonProps {
   onPress: () => void;
 }
 
+const modalStyle = {maxWidth: 450};
+
+const ExampleButton: React.FC<ExampleButtonProps> = ({
+  title,
+  onPress,
+}: ExampleButtonProps) => (
+  <TouchableOpacity onPress={onPress}>
+    <ExampleText>{title}</ExampleText>
+  </TouchableOpacity>
+);
+
+const AlertExample: React.FC<{
+  setDialogResult: React.Dispatch<
+    React.SetStateAction<string | boolean | null>
+  >;
+}> = ({setDialogResult}) => {
+  const alertRef = useRef<Modal>(null);
+
+  return (
+    <>
+      <ExampleButton
+        title={'Alert'}
+        onPress={() => {
+          alertRef.current?.open();
+        }}
+      />
+      <AlertDialog
+        ref={alertRef}
+        style={modalStyle}
+        title={'Dialogue Title'}
+        content={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+        onPress={(result) => {
+          setDialogResult(result);
+          alertRef.current?.close();
+        }}
+      />
+    </>
+  );
+};
+
+const ConfirmExample: React.FC<{
+  setDialogResult: React.Dispatch<
+    React.SetStateAction<string | boolean | null>
+  >;
+}> = ({setDialogResult}) => {
+  const confirmRef = useRef<Modal>(null);
+
+  return (
+    <>
+      <ExampleButton
+        title={'Confirm'}
+        onPress={() => {
+          confirmRef.current?.open();
+        }}
+      />
+      <AlertDialog
+        type={'confirm'}
+        ref={confirmRef}
+        style={modalStyle}
+        title={'Dialogue Title'}
+        content={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+        onPress={(result) => {
+          setDialogResult(result);
+          confirmRef.current?.close();
+        }}
+      />
+    </>
+  );
+};
+
+const PromptExample: React.FC<{
+  setDialogResult: React.Dispatch<
+    React.SetStateAction<string | boolean | null>
+  >;
+}> = ({setDialogResult}) => {
+  const promptRef = useRef<Modal>(null);
+
+  return (
+    <>
+      <ExampleButton
+        title={'Prompt'}
+        onPress={() => {
+          promptRef.current?.open();
+        }}
+      />
+      <AlertDialog
+        type={'prompt'}
+        ref={promptRef}
+        style={modalStyle}
+        content={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+        onPress={(result) => {
+          setDialogResult(result);
+          promptRef.current?.close();
+        }}
+      />
+    </>
+  );
+};
+
 function Dialog(): React.ReactElement {
   const [dialogResult, setDialogResult] = useState<string | boolean | null>('');
 
-  const alertRef = useRef<Modal>(null);
-  const confirmRef = useRef<Modal>(null);
-  const promptRef = useRef<Modal>(null);
-
-  const modalStyle = {maxWidth: 450};
-
-  const ExampleButton: React.FC<ExampleButtonProps> = ({
-    title,
-    onPress,
-  }: ExampleButtonProps) => (
-    <TouchableOpacity onPress={onPress}>
-      <ExampleText>{title}</ExampleText>
-    </TouchableOpacity>
-  );
-
-  const AlertExample: React.FC = () => {
-    return (
-      <>
-        <ExampleButton
-          title={'Alert'}
-          onPress={() => {
-            alertRef.current?.open();
-          }}
-        />
-        <AlertDialog
-          ref={alertRef}
-          style={modalStyle}
-          title={'Dialogue Title'}
-          content={
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          }
-          onPress={(result) => {
-            setDialogResult(result);
-            alertRef.current?.close();
-          }}
-        />
-      </>
-    );
-  };
-
-  const ConfirmExample: React.FC = () => {
-    return (
-      <>
-        <ExampleButton
-          title={'Confirm'}
-          onPress={() => {
-            confirmRef.current?.open();
-          }}
-        />
-        <AlertDialog
-          type={'confirm'}
-          ref={confirmRef}
-          style={modalStyle}
-          title={'Dialogue Title'}
-          content={
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          }
-          onPress={(result) => {
-            setDialogResult(result);
-            confirmRef.current?.close();
-          }}
-        />
-      </>
-    );
-  };
-
-  const PromptExample: React.FC = () => {
-    return (
-      <>
-        <ExampleButton
-          title={'Prompt'}
-          onPress={() => {
-            promptRef.current?.open();
-          }}
-        />
-        <AlertDialog
-          type={'prompt'}
-          ref={promptRef}
-          style={modalStyle}
-          content={
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          }
-          onPress={(result) => {
-            setDialogResult(result);
-            promptRef.current?.close();
-          }}
-        />
-      </>
-    );
-  };
-
   return (
     <Container>
-      <AlertExample />
-      <ConfirmExample />
-      <PromptExample />
+      <AlertExample setDialogResult={setDialogResult} />
+      <ConfirmExample setDialogResult={setDialogResult} />
+      <PromptExample setDialogResult={setDialogResult} />
       <ResultText>{`Dialog result : ${dialogResult}`}</ResultText>
     </Container>
   );
