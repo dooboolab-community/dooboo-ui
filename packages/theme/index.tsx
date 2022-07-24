69744ac33154634cdd30b88665ee1a113dfe5ab3
hyochan <dooboolab@gmail.com>
2022-07-24 15:04:39 +0900

    Rename theme provider

--- packages/theme/index.tsx
+++ packages/theme/index.tsx
@@ -1,2 +1,2 @@
-export {ThemeProvider, useTheme, withTheme} from './Themeprovider';
+export {ThemeProvider, useTheme, withTheme} from './ThemeProviders';
 export {DoobooTheme, ThemeType, dark, light} from './colors';
