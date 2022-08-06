d02aaa17ec6b0f00ea996cdce00dfbc6592d7c50
hyochan <dooboolab@gmail.com>
2022-08-06 22:02:14 +0900

    docs: add progressbar component to doc site

---
+++ docs/src/components/Progressbar/index.tsx
@@ -0,0 +1,37 @@
+import {Progressbar, ProgressbarStyles, ProgressbarType} from 'dooboo-ui';
+import React, {FC, useEffect, useState} from 'react';
+
+type Props = {
+  type: ProgressbarType;
+  styles?: ProgressbarStyles;
+};
+
+const ProgressbarDefault: FC<Props> = ({type, styles}) => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    const timeoutCallback = (): void => {
+      setValue((prevProgress: number): number => {
+        const nextProgress = prevProgress + 1;
+
+        timeout = setTimeout(timeoutCallback, 100);
+
+        if (prevProgress >= 100) {
+          return 0;
+        }
+
+        return nextProgress;
+      });
+    };
+
+    timeout = setTimeout(timeoutCallback, 100);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return <Progressbar value={value} type={type} styles={styles} />;
+};
+
+export default ProgressbarDefault;
