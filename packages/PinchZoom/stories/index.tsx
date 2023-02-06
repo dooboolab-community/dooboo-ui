261f586db2cf5ad84ae26c6e0aff38f067601248
hyochan <dooboolab@gmail.com>
2023-02-06 11:17:34 +0900

    chore: typo

--- packages/PinchZoom/stories/index.tsx
+++ packages/PinchZoom/stories/index.tsx
@@ -2,4 +2,6 @@ import {ImageSlider} from './PinchZoomSlider';
 import React from 'react';
 import {storiesOf} from '@storybook/react-native';
 
-storiesOf('[Package] ', module).add('Basic - light', () => <ImageSlider />);
+storiesOf('[Package] ImageSlider', module).add('Basic - light', () => (
+  <ImageSlider />
+));
