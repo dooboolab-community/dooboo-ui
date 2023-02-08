42e285a7a3fa0b772d0ef962c16206e37249ef7f
hyochan <dooboolab@gmail.com>
2023-02-08 16:46:35 +0900

    fix: migrate [FAB] doc which breaks

--- docs/src/components/FAB/index.tsx
+++ docs/src/components/FAB/index.tsx
@@ -1,13 +1,12 @@
 import {DoobooProvider, FAB} from 'dooboo-ui';
-import {SafeAreaView, View} from 'react-native';
 
 import type {ReactElement} from 'react';
+import {View} from 'react-native';
 import styled from '@emotion/native';
 import {useFonts} from 'expo-font';
 import {useState} from 'react';
 
 const StoryContainer = styled.View`
-  flex: 1;
   width: 100%;
   height: 300px;
   align-self: stretch;
@@ -27,18 +26,16 @@ function FABStory(): ReactElement {
 
   return (
     <StoryContainer>
-      <SafeAreaView style={{display: 'flex', width: '100%', height: '100%'}}>
-        <FAB
-          styles={{buttonSize: 'medium'}}
-          isActive={active}
-          FABItems={[
-            {id: 'search', icon: 'home-light'},
-            {id: 'like', icon: 'like-light'},
-          ]}
-          onPressFAB={() => setActive((prev) => !prev)}
-          onPressFABItem={() => {}}
-        />
-      </SafeAreaView>
+      <FAB
+        styles={{buttonSize: 'medium'}}
+        isActive={active}
+        items={[
+          {id: 'search', icon: 'home-light'},
+          {id: 'like', icon: 'like-light'},
+        ]}
+        onPressFAB={() => setActive((prev) => !prev)}
+        onPressItem={() => {}}
+      />
     </StoryContainer>
   );
 }
