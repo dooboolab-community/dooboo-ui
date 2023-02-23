da38284214198700af748c052fed3dc14f1d7543
jw12 <ujw97@naver.com>
2023-02-24 01:03:02 +0900

    fix: apply storybook-dark-mode to loading indicator

--- docs/src/components/LoadingIndicator/index.tsx
+++ docs/src/components/LoadingIndicator/index.tsx
@@ -1,3 +1,42 @@
-export * from './Basic';
-export * from './ImageVersion';
-export * from '../Button/Link';
+import React, {ReactElement, ReactNode, useEffect} from 'react';
+import styled from '@emotion/native/dist/emotion-native.cjs';
+import {DoobooProvider, useDooboo} from 'dooboo-ui';
+import {useDarkMode} from 'storybook-dark-mode';
+
+export {default as Basic} from './Basic';
+export {default as ImageVersion} from './ImageVersion';
+export {default as Link} from '../Button/Link';
+
+type ContainerProps = {
+  children: ReactNode;
+};
+
+const StoryContainer = styled.View`
+  background-color: ${({theme}) => theme.bg.basic};
+  justify-content: center;
+  align-items: center;
+`;
+
+export function StoryWrapper({children}: ContainerProps): ReactElement {
+  const {themeType, changeThemeType} = useDooboo();
+  const isDark = useDarkMode();
+  const storybookTheme = isDark ? 'dark' : 'light';
+
+  useEffect(() => {
+    if (storybookTheme !== themeType) {
+      changeThemeType();
+    }
+  }, [storybookTheme]);
+
+  return <StoryContainer>{children}</StoryContainer>;
+}
+
+export function StoryProvider({children}: ContainerProps): ReactElement {
+  const isDark = useDarkMode();
+
+  return (
+    <DoobooProvider themeConfig={{initialThemeType: isDark ? 'dark' : 'light'}}>
+      <StoryWrapper>{children}</StoryWrapper>
+    </DoobooProvider>
+  );
+}
