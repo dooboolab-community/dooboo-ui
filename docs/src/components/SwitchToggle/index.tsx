ea0f4f0bdf6756d114d71139767767e33fe367b8
jw12 <ujw97@naver.com>
2023-02-24 01:41:10 +0900

    fix: apply storybook-dark-mode to switch toggle

--- docs/src/components/SwitchToggle/index.tsx
+++ docs/src/components/SwitchToggle/index.tsx
@@ -1,1 +1,95 @@
-export * from './Basic';
+import React, {ReactElement, useEffect, useState} from 'react';
+import styled, {css} from '@emotion/native';
+import {DoobooProvider, SwitchToggle, Typography, useDooboo} from 'dooboo-ui';
+import {useDarkMode} from 'storybook-dark-mode';
+import {View} from 'react-native';
+
+const StoryContainer = styled.View`
+  background-color: ${({theme}) => theme.bg.basic};
+  justify-content: center;
+  align-items: center;
+`;
+
+export function StoryWrapper(): ReactElement {
+  const {themeType, changeThemeType} = useDooboo();
+  const isDark = useDarkMode();
+  const storybookTheme = isDark ? 'dark' : 'light';
+
+  const [isOn, setIsOn] = useState(false);
+
+  useEffect(() => {
+    if (storybookTheme !== themeType) {
+      changeThemeType();
+    }
+  }, [storybookTheme]);
+
+  return (
+    <StoryContainer>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Typography.Body1>{!isOn ? 'On' : 'Off'}</Typography.Body1>
+          <SwitchToggle
+            styles={{
+              container: css`
+                margin-left: 8px;
+                width: 36px;
+                height: 21px;
+                border-radius: 50px;
+                border-width: 2px;
+                padding: 4px;
+                border-width: 0px;
+              `,
+              circle: css`
+                width: 12px;
+                height: 12px;
+                border-radius: 50px;
+              `,
+            }}
+            isOn={!isOn}
+            onPress={() => setIsOn(!isOn)}
+          />
+        </View>
+        <View
+          style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}
+        >
+          <Typography.Body1>{!isOn ? 'On' : 'Off'}</Typography.Body1>
+          <SwitchToggle
+            styles={{
+              container: css`
+                margin-left: 8px;
+                width: 36px;
+                height: 21px;
+                border-radius: 50px;
+                border-width: 2px;
+                padding: 4px;
+                border-width: 0px;
+              `,
+              circle: css`
+                width: 12px;
+                height: 12px;
+                border-radius: 50px;
+              `,
+            }}
+            isOn={!isOn}
+            onPress={() => setIsOn(!isOn)}
+          />
+        </View>
+      </View>
+    </StoryContainer>
+  );
+}
+
+export default function StoryProvider(): ReactElement {
+  const isDark = useDarkMode();
+
+  return (
+    <DoobooProvider themeConfig={{initialThemeType: isDark ? 'dark' : 'light'}}>
+      <StoryWrapper />
+    </DoobooProvider>
+  );
+}
