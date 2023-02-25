adb7e110bc402eb4536f6f60671c3d445f077fc2
ujw0712 <ujw97@naver.com>
2023-02-25 22:39:36 +0900

    fix: remove duplicate code in button sizes

--- docs/src/components/Button/Sizes.tsx
+++ docs/src/components/Button/Sizes.tsx
@@ -1,42 +1,32 @@
-import {Button, useDooboo} from 'dooboo-ui';
+import type {ButtonSizeType} from 'dooboo-ui';
+import {Button} from 'dooboo-ui';
 import type {ReactElement} from 'react';
 import {View} from 'react-native';
 import {StoryProvider} from './index';
 
 export default function Sizes(): ReactElement {
+  const sizes: ButtonSizeType[] = ['large', 'medium', 'small'];
+
   return (
     <StoryProvider>
       <View
         style={{
           marginVertical: 8,
-
           flexDirection: 'row',
           flexWrap: 'wrap',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <Button
-          color="primary"
-          size="large"
-          text="Primary"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          color="primary"
-          size="medium"
-          text="Primary"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          color="primary"
-          size="small"
-          text="Primary"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
+        {sizes.map((size) => (
+          <Button
+            color="primary"
+            size={size}
+            text={size}
+            style={{margin: 8}}
+            onPress={() => {}}
+          />
+        ))}
       </View>
     </StoryProvider>
   );
