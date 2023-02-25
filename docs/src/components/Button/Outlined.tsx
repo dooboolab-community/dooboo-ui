f5c5098fc65526d4afb7c8fe9d8239974c12e2be
ujw0712 <ujw97@naver.com>
2023-02-25 22:38:24 +0900

    fix: remove duplicate code in outlined button

--- docs/src/components/Button/Outlined.tsx
+++ docs/src/components/Button/Outlined.tsx
@@ -1,69 +1,39 @@
 import {Button} from 'dooboo-ui';
+import type {ButtonColorType} from 'dooboo-ui';
 import type {ReactElement} from 'react';
 import {View} from 'react-native';
 import {StoryProvider} from './index';
 
 export default function Outlined(): ReactElement {
+  const colors: ButtonColorType[] = [
+    'primary',
+    'secondary',
+    'success',
+    'warning',
+    'danger',
+    'info',
+    'light',
+  ];
+
   return (
     <StoryProvider>
       <View
         style={{
           marginVertical: 8,
-
           flexDirection: 'row',
           flexWrap: 'wrap',
           justifyContent: 'center',
         }}
       >
-        <Button
-          type="outlined"
-          color="primary"
-          text="Primary"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          type="outlined"
-          color="secondary"
-          text="Secondary"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          color="success"
-          text="Success"
-          type="outlined"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          color="danger"
-          text="Danger"
-          type="outlined"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          color="warning"
-          text="Warning"
-          type="outlined"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          color="info"
-          text="Info"
-          type="outlined"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
-        <Button
-          color="light"
-          text="Light"
-          type="outlined"
-          style={{margin: 8}}
-          onPress={() => {}}
-        />
+        {colors.map((color) => (
+          <Button
+            type="outlined"
+            color={color}
+            text={color}
+            style={{margin: 8}}
+            onPress={() => {}}
+          />
+        ))}
       </View>
     </StoryProvider>
   );
