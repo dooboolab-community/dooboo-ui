e2a3dbe69ffb31eb5dc9492fcb714b6970687772
hyochan <dooboolab@gmail.com>
2023-03-07 17:43:30 +0900

    feat: add more typographies

--- main/uis/Typography/Typography.tsx
+++ main/uis/Typography/Typography.tsx
@@ -22,7 +22,7 @@ export const Title = withTheme(StyledTitle);
 
 // Heading1
 const StyledHeading1 = styled.Text<{theme: DoobooTheme}>`
-  font-size: 22px;
+  font-size: 26px;
   font-weight: 400;
   color: ${({theme}) => {
     if (isEmptyObject(theme)) {
@@ -39,7 +39,7 @@ export const Heading1 = withTheme(StyledHeading1);
 
 // Heading2
 const StyledHeading2 = styled.Text<{theme: DoobooTheme}>`
-  font-size: 17px;
+  font-size: 22px;
   font-weight: 400;
   color: ${({theme}) => {
     if (isEmptyObject(theme)) {
@@ -56,7 +56,7 @@ export const Heading2 = withTheme(StyledHeading2);
 
 // Heading3
 const StyledHeading3 = styled.Text<{theme: DoobooTheme}>`
-  font-size: 16px;
+  font-size: 18px;
   font-weight: 400;
   color: ${({theme}) => {
     if (isEmptyObject(theme)) {
@@ -71,9 +71,43 @@ StyledHeading3.defaultProps = {style: {includeFontPadding: false}};
 
 export const Heading3 = withTheme(StyledHeading3);
 
+// Heading4
+const StyledHeading4 = styled.Text<{theme: DoobooTheme}>`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({theme}) => {
+    if (isEmptyObject(theme)) {
+      return theme.text.disabled;
+    }
+
+    return theme.text.basic;
+  }};
+`;
+
+StyledHeading4.defaultProps = {style: {includeFontPadding: false}};
+
+export const Heading4 = withTheme(StyledHeading4);
+
+// Heading5
+const StyledHeading5 = styled.Text<{theme: DoobooTheme}>`
+  font-size: 14px;
+  font-weight: 400;
+  color: ${({theme}) => {
+    if (isEmptyObject(theme)) {
+      return theme.text.disabled;
+    }
+
+    return theme.text.basic;
+  }};
+`;
+
+StyledHeading5.defaultProps = {style: {includeFontPadding: false}};
+
+export const Heading5 = withTheme(StyledHeading5);
+
 // Body1
 const StyledBody1 = styled.Text<{theme: DoobooTheme}>`
-  font-size: 16px;
+  font-size: 18px;
   color: ${({theme}) => {
     if (isEmptyObject(theme)) {
       return theme.text.disabled;
@@ -89,7 +123,7 @@ export const Body1 = withTheme(StyledBody1);
 
 // Body2
 const StyledBody2 = styled.Text<{theme: DoobooTheme}>`
-  font-size: 14px;
+  font-size: 16px;
   color: ${({theme}) => {
     if (isEmptyObject(theme)) {
       return theme.text.disabled;
@@ -102,3 +136,35 @@ const StyledBody2 = styled.Text<{theme: DoobooTheme}>`
 StyledBody2.defaultProps = {style: {includeFontPadding: false}};
 
 export const Body2 = withTheme(StyledBody2);
+
+// Body3
+const StyledBody3 = styled.Text<{theme: DoobooTheme}>`
+  font-size: 14px;
+  color: ${({theme}) => {
+    if (isEmptyObject(theme)) {
+      return theme.text.disabled;
+    }
+
+    return theme.text.basic;
+  }};
+`;
+
+StyledBody3.defaultProps = {style: {includeFontPadding: false}};
+
+export const Body3 = withTheme(StyledBody3);
+
+// Body4
+const StyledBody4 = styled.Text<{theme: DoobooTheme}>`
+  font-size: 14px;
+  color: ${({theme}) => {
+    if (isEmptyObject(theme)) {
+      return theme.text.disabled;
+    }
+
+    return theme.text.basic;
+  }};
+`;
+
+StyledBody4.defaultProps = {style: {includeFontPadding: false}};
+
+export const Body4 = withTheme(StyledBody4);
