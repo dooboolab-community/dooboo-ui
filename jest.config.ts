fb3a53ecc7926d57da873fcafd337ccb078f3003
hyochan <dooboolab@gmail.com>
2023-03-19 21:03:01 +0900

    test: new jest config by expo

--- jest.config.ts
+++ jest.config.ts
@@ -40,7 +40,7 @@ export default async (): Promise<Config.InitialOptions> => {
     },
     transformIgnorePatterns: [
       // eslint-disable-next-line max-len
-      'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|dooboo-ui|@dooboo-ui)',
+      'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|dooboo-ui|@dooboo-ui)',
     ],
     coveragePathIgnorePatterns: [
       '.example.',
