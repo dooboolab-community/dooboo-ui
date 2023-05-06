752e46b36521450be41186ccf9667ed187d2ac79
hyochan <dooboolab@gmail.com>
2023-05-07 05:46:42 +0900

    refactor: migrate react-docgen-typescript to args table

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -10,5 +10,5 @@ module.exports = {
       },
     },
     'storybook-dark-mode',
-  ],
+  ]
 };
--- docs/babel.config.cjs
+++ docs/babel.config.cjs
@@ -11,6 +11,7 @@ module.exports = (api) => {
       ],
     ],
     plugins: [
+      'react-docgen',
       'react-require',
       'react-native-web',
       '@babel/plugin-syntax-dynamic-import',
