d4fa87bcd377d9c65b2841e9632f2119f7092526
hyochan <dooboolab@gmail.com>
2023-07-17 00:37:40 +0900

    build: update pkgs

--- main/uis/Typography/Typography.tsx
+++ main/uis/Typography/Typography.tsx
@@ -36,40 +36,30 @@ const StyledHeading = styled.Text<{theme: DoobooTheme}>`
 
 StyledHeading.defaultProps = {style: {includeFontPadding: false}};
 
-export const Heading1 = withTheme(
-  styled(StyledHeading)`
-    font-size: 28px;
-    line-height: 39.2px;
-  `,
-);
+export const Heading1 = withTheme(styled(StyledHeading)`
+  font-size: 28px;
+  line-height: 39.2px;
+`);
 
-export const Heading2 = withTheme(
-  styled(StyledHeading)`
-    font-size: 26px;
-    line-height: 36.4px;
-  `,
-);
+export const Heading2 = withTheme(styled(StyledHeading)`
+  font-size: 26px;
+  line-height: 36.4px;
+`);
 
-export const Heading3 = withTheme(
-  styled(StyledHeading)`
-    font-size: 24px;
-    line-height: 33.6px;
-  `,
-);
+export const Heading3 = withTheme(styled(StyledHeading)`
+  font-size: 24px;
+  line-height: 33.6px;
+`);
 
-export const Heading4 = withTheme(
-  styled(StyledHeading)`
-    font-size: 22px;
-    line-height: 30.8px;
-  `,
-);
+export const Heading4 = withTheme(styled(StyledHeading)`
+  font-size: 22px;
+  line-height: 30.8px;
+`);
 
-export const Heading5 = withTheme(
-  styled(StyledHeading)`
-    font-size: 20px;
-    line-height: 28px;
-  `,
-);
+export const Heading5 = withTheme(styled(StyledHeading)`
+  font-size: 20px;
+  line-height: 28px;
+`);
 
 // Body
 const StyledBody = styled.Text<{theme: DoobooTheme}>`
@@ -85,30 +75,22 @@ const StyledBody = styled.Text<{theme: DoobooTheme}>`
 
 StyledBody.defaultProps = {style: {includeFontPadding: false}};
 
-export const Body1 = withTheme(
-  styled(StyledBody)`
-    font-size: 18px;
-    line-height: 25.2px;
-  `,
-);
+export const Body1 = withTheme(styled(StyledBody)`
+  font-size: 18px;
+  line-height: 25.2px;
+`);
 
-export const Body2 = withTheme(
-  styled(StyledBody)`
-    font-size: 16px;
-    line-height: 22.4px;
-  `,
-);
+export const Body2 = withTheme(styled(StyledBody)`
+  font-size: 16px;
+  line-height: 22.4px;
+`);
 
-export const Body3 = withTheme(
-  styled(StyledBody)`
-    font-size: 14px;
-    line-height: 19.6px;
-  `,
-);
+export const Body3 = withTheme(styled(StyledBody)`
+  font-size: 14px;
+  line-height: 19.6px;
+`);
 
-export const Body4 = withTheme(
-  styled(StyledBody)`
-    font-size: 12px;
-    line-height: 16.4px;
-  `,
-);
+export const Body4 = withTheme(styled(StyledBody)`
+  font-size: 12px;
+  line-height: 16.4px;
+`);
--- main/uis/Typography/TypographyInverted.tsx
+++ main/uis/Typography/TypographyInverted.tsx
@@ -36,40 +36,30 @@ const InvertedStyledHeading = styled.Text<{theme: DoobooTheme}>`
 
 InvertedStyledHeading.defaultProps = {style: {includeFontPadding: false}};
 
-export const InvertedHeading1 = withTheme(
-  styled(InvertedStyledHeading)`
-    font-size: 28px;
-    line-height: 39.2px;
-  `,
-);
+export const InvertedHeading1 = withTheme(styled(InvertedStyledHeading)`
+  font-size: 28px;
+  line-height: 39.2px;
+`);
 
-export const InvertedHeading2 = withTheme(
-  styled(InvertedStyledHeading)`
-    font-size: 26px;
-    line-height: 36.4px;
-  `,
-);
+export const InvertedHeading2 = withTheme(styled(InvertedStyledHeading)`
+  font-size: 26px;
+  line-height: 36.4px;
+`);
 
-export const InvertedHeading3 = withTheme(
-  styled(InvertedStyledHeading)`
-    font-size: 24px;
-    line-height: 33.6px;
-  `,
-);
+export const InvertedHeading3 = withTheme(styled(InvertedStyledHeading)`
+  font-size: 24px;
+  line-height: 33.6px;
+`);
 
-export const InvertedHeading4 = withTheme(
-  styled(InvertedStyledHeading)`
-    font-size: 22px;
-    line-height: 30.8px;
-  `,
-);
+export const InvertedHeading4 = withTheme(styled(InvertedStyledHeading)`
+  font-size: 22px;
+  line-height: 30.8px;
+`);
 
-export const InvertedHeading5 = withTheme(
-  styled(InvertedStyledHeading)`
-    font-size: 20px;
-    line-height: 28px;
-  `,
-);
+export const InvertedHeading5 = withTheme(styled(InvertedStyledHeading)`
+  font-size: 20px;
+  line-height: 28px;
+`);
 
 // Body
 const InvertedStyledBody = styled.Text<{theme: DoobooTheme}>`
@@ -85,30 +75,22 @@ const InvertedStyledBody = styled.Text<{theme: DoobooTheme}>`
 
 InvertedStyledBody.defaultProps = {style: {includeFontPadding: false}};
 
-export const InvertedBody1 = withTheme(
-  styled(InvertedStyledBody)`
-    font-size: 18px;
-    line-height: 25.2px;
-  `,
-);
+export const InvertedBody1 = withTheme(styled(InvertedStyledBody)`
+  font-size: 18px;
+  line-height: 25.2px;
+`);
 
-export const InvertedBody2 = withTheme(
-  styled(InvertedStyledBody)`
-    font-size: 16px;
-    line-height: 22.4px;
-  `,
-);
+export const InvertedBody2 = withTheme(styled(InvertedStyledBody)`
+  font-size: 16px;
+  line-height: 22.4px;
+`);
 
-export const InvertedBody3 = withTheme(
-  styled(InvertedStyledBody)`
-    font-size: 14px;
-    line-height: 19.6px;
-  `,
-);
+export const InvertedBody3 = withTheme(styled(InvertedStyledBody)`
+  font-size: 14px;
+  line-height: 19.6px;
+`);
 
-export const InvertedBody4 = withTheme(
-  styled(InvertedStyledBody)`
-    font-size: 12px;
-    line-height: 16.4px;
-  `,
-);
+export const InvertedBody4 = withTheme(styled(InvertedStyledBody)`
+  font-size: 12px;
+  line-height: 16.4px;
+`);
