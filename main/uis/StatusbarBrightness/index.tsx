8cb299ca454cccd9cc75469e52f41ceaf99e50b3
hyochan <dooboolab@gmail.com>
2024-06-30 12:54:25 +0900

    feat: add type on [StatusbarBrightness]

--- main/uis/StatusbarBrightness/index.tsx
+++ main/uis/StatusbarBrightness/index.tsx
@@ -2,11 +2,14 @@ import type {StatusBarStyle} from 'react-native';
 import {StatusBar} from 'react-native';
 import {useTheme} from '@dooboo-ui/theme';
 
-function StatusBarBrightness(): JSX.Element {
+function StatusBarBrightness({type}: {type?: StatusBarStyle}): JSX.Element {
   const {themeType} = useTheme();
 
-  const statusColor: StatusBarStyle =
-    themeType === 'light' ? 'dark-content' : 'light-content';
+  const statusColor: StatusBarStyle = type
+    ? type
+    : themeType === 'light'
+      ? 'dark-content'
+      : 'light-content';
 
   return <StatusBar barStyle={statusColor} />;
 }
